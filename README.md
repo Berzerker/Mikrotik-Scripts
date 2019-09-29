1d26c3ca9ad5e690ad34c60f59b7dfc0efa71280
Berzerker <a.akker214@gmail.com>
2019-09-29 15:56:55 -0400

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # Mikrotik-Scripts
 Useful scripts for use as an edge RouterOS device
+
+# Installation
+
+1. In the RouterOS WebGUI (doing this via CLI will make you want to do unspeakable things to people), System > Scripts > Add New
+
+2. Follow the configuration requirements for each script below
+
+## Cloudflare-DDNS
+
+1. Install python-cloudflare
+
+`$ sudo apt install python-pip && sudo pip install cloudflare`
+
+2. Edit the cloudflare-api.py to enter your domain you want to find the IDs for, your email, and global API key
+
+3. Run the cloudflare-api.py to get your zone IDs and record IDs
+
+4. Edit the RouterOS script according to your domains and specifications
+
+`$ python cloudflare-api.py`
+
+RouterOS Config Requirements:
+```
+Don't Require Permissions: UNCHECKED
+Policy: Read, Write, Test
+```
+
+## wake-on-lan
+
+1. Edit the RouterOS according to the interface the device is on and the MAC Address of the NIC
+
+RouterOS Config Requirements:
+```
+Don't Require Permissions: UNCHECKED
+Policy: Read, Test
+```
---
+++ cloudflare-api.py
@@ -0,0 +1,44 @@
+import CloudFlare
+
+def main():
+
+    zone_name = 'domain.com'
+
+    cf = CloudFlare.CloudFlare(email='emailaddress', token='globalAPIkey')
+
+# query for the zone name and expect only one value back
+    try:
+        zones = cf.zones.get(params = {'name':zone_name,'per_page':1})
+    except CloudFlare.exceptions.CloudFlareAPIError as e:
+        exit('/zones.get %d %s - api call failed' % (e, e))
+    except Exception as e:
+        exit('/zones.get - %s - api call failed' % (e))
+
+    if len(zones) == 0:
+        exit('No zones found')
+
+    # extract the zone_id which is needed to process that zone
+    zone = zones[0]
+    zone_id = zone['id']
+
+    # request the DNS records from that zone
+    try:
+        dns_records = cf.zones.dns_records.get(zone_id)
+    except CloudFlare.exceptions.CloudFlareAPIError as e:
+        exit('/zones/dns_records.get %d %s - api call failed' % (e, e))
+
+    # print the results - first the zone name
+    print zone_id, zone_name
+
+    # then all the DNS records for that zone
+    for dns_record in dns_records:
+        r_name = dns_record['name']
+        r_type = dns_record['type']
+        r_value = dns_record['content']
+        r_id = dns_record['id']
+        print '\t', r_id, r_name, r_type, r_value
+
+    exit(0)
+
+if __name__ == '__main__':
+    main()
